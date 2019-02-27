b26d7aa5dfe9034cb2aab867e916ee59548693e3
Rui Ribeiro <rui.ribeiro@sincelo.pt>
2019-02-27 08:59:39 +0000

    testes

--- ArduinoEthernetAC/ArduinoEthernetAC.ino
+++ ArduinoEthernetAC/ArduinoEthernetAC.ino
@@ -1,8 +1,15 @@
+#include <Ethernet.h>
+#include <SPI.h>
 #include <EEPROM.h>
 
 int go=0;
 byte mac[6] = { 0xBA, 0xBE, 0x00, 0x00, 0x00, 0x00 };
 char macstr[18];
+IPAddress ip;
+IPAddress ipzero=IPAddress(0,0,0,0);
+String Resultado="";
+EthernetClient client;
+String PostData="";
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -23,19 +30,56 @@ void setup() {
   snprintf(macstr, 18, "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
   // Start up networking
-  Serial.print("DHCP");
+  Serial.print("DHCP (");
   Serial.print(macstr);
-  Serial.print(")...");
-//  Ethernet.begin(mac);
-  Serial.print("success");
-//  Serial.println(Ethernet.localIP());
+  Serial.println(")...");
+  Ethernet.begin(mac);
+  Serial.println("success");
+  ip=Ethernet.localIP();
+  Serial.println(ip);
+  if(ip==ipzero) {
+    Serial.println("no ip");
+    Ethernet.begin(mac, IPAddress(192,168,2,2), IPAddress(8,8,8,8), IPAddress(192,168,2,1), IPAddress(255,255,255,0)); 
+    Serial.println(Ethernet.localIP());
+  }
+//  Ethernet.begin(mac, ip, dns, gateway, subnet); 
+  Serial.println("go");
 }
 
 void loop() {
-  if (go == 1) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+  if(client.available()) {
+    char c=client.read();
+    Resultado=Resultado+c;
+    if(!client.available()) {
+      Serial.println("disconnecting.");
+      client.stop();
+      Serial.print(Resultado);
+      }
+      go=0;
+  } else {
+    if(go==0)
+    {
+      go=1;
+      Serial.println("connecting...");
+      if (client.connect("192.168.2.1", 8888)) {
+        Serial.println("connected");
+        PostData="&hex=1&tiporegisto=Ambos&terminal=1&ip="+ip;
+        client.println("POST /validacartao.php HTTP/1.1");
+        client.println("Host: 192.168.2.2");
+        client.println("User-Agent: Arduino/1.0");
+        client.println("Connection: close");
+        client.println("Content-Type: application/x-www-form-urlencoded;");
+        client.print("Content-Length: ");
+        client.println(PostData.length());
+        client.println();
+        client.println(PostData);
+        Resultado="";
+      }
+      else {
+        Serial.println("connection failed");
+      }
+    }
   }
+
+  
 }
